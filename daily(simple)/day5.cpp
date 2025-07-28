0276ba0e70102705395170e77da438466ebeb548
xdmandy <mandardolas9756@gmail.com>
2025-07-28 19:19:01 +0530

    day 5 problem 2

--- daily(simple)/day5.cpp
+++ daily(simple)/day5.cpp
@@ -129,3 +129,125 @@ int main() {
     }
 
 }
+
+
+/*
+MIN To MAX
+You are given an array A of size N
+Let M be the minimum value present in the array initially.
+In one operation, you can choose an element Ai(1≤i≤N and an integer 
+(1≤X≤100), and set Ai =X.
+
+Determine the minimum number of operations required to make M the maximum value in the array 
+A.
+
+Input Format
+The first line of input will contain a single integer 
+T
+T, denoting the number of test cases.
+Each test case consists of multiple lines of input.
+The first line of each test case contains a single integer 
+N
+N - the size of the array.
+The next line of each test case contains 
+N
+N space-separated integers 
+A
+1
+,
+A
+2
+,
+…
+,
+A
+N
+A 
+1
+​
+ ,A 
+2
+​
+ ,…,A 
+N
+​
+  - the elements of the array.
+Output Format
+For each test case, output on a new line, the minimum number of operations required to make 
+M
+M the maximum value in the array 
+A
+A.
+Constraints
+1
+≤
+T
+≤
+100
+1≤T≤100
+1
+≤
+N
+≤
+100
+1≤N≤100
+1
+≤
+A
+i
+≤
+100
+1≤A 
+i
+​
+ ≤100
+Sample 1:
+Input
+Output
+3
+2
+1 2
+4
+2 2 3 4
+1
+1
+1
+2
+0
+*/
+
+//solution
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int a[n];
+        int min=100;
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+            if(a[i]<min) min=a[i];
+        }
+        
+        
+        // your code goes here
+        int count=0;
+        for (int ops : a) {
+            if(ops>min){ 
+                ops=min; 
+                count++;
+        }
+        
+    }
+    
+    cout << count << endl;
+    
+
+}
+}
