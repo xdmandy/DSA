27a5d56a0c22c6819024805cb19a3f1641cebbb0
xdmandy <mandardolas9756@gmail.com>
2025-08-05 01:34:52 +0530

    q&a

--- daily(simple)/day7.cpp
+++ daily(simple)/day7.cpp
@@ -163,3 +163,109 @@ int main() {
 }
 
 }
+
+
+/*
+ind the peak elements in an array
+Given an array A of size N, your task is to find and print all the peak elements in the array. A peak element is one that is strictly greater than its neighboring elements. For the first and last elements, only consider their single adjacent element.
+
+If no peak element exists in the array, print -1.
+
+Input Format
+The first line contains the integer 
+N
+N — the size of array
+The second line contains all the elements of array 
+A
+A
+Output Format
+Output all the peak elements in the array in the order they are present in the original array.
+
+Constraints
+1
+≤
+N
+≤
+10
+5
+1≤N≤10 
+5
+ 
+1
+≤
+A
+i
+≤
+10
+5
+1≤A 
+i
+​
+ ≤10 
+5
+ 
+Sample 1:
+Input
+
+5
+1 2 4 3 1
+Output
+4
+Explanation:
+1 is smaller than it's adjacent element 2.
+2 is greater than 1 but smaller than 4.
+4 is greater than both 2 and 3, thus it is a peak element.
+Again 3 and 1 are also smaller than their adjacent elements.
+Thus the output is only 4.
+*/
+
+//solution
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int n;
+	cin >> n;
+	int arr[n];
+	for(int i=0;i<n;i++){
+	   cin>>arr[i];
+	}
+	
+    bool found=0;
+    for(int i=0;i<n;i++){
+        
+      
+        
+        if(n == 1){
+            
+            cout << arr[i] <<" "; 
+            found=1; 
+        
+        }else if(i==0 && arr[i]>arr[i+1]){
+            
+            cout<<arr[i]<<" ";
+            found=1;
+        
+        }else if(i!=0 && arr[i]>arr[i+1] && arr[i]>arr[i-1]){
+            
+            cout<<arr[i]<<" ";
+            found=1;
+        }
+        else if(i==n-1 && arr[i]>arr[i-1]){
+            
+            cout<<arr[i]<<" ";
+            found=1;
+        
+        }
+    
+   
+    }
+    
+    if(!found){
+        cout<<"-1";
+    
+    
+}
+}
